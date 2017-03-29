a175637dc3367675ef0e479fb222b217e3b5defc
Andres Gomez Casanova <angoca@yahoo.com>
2017-03-29 16:11:31 -0500

    Update update.sh

--- travis/update.sh
+++ travis/update.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo "Architecture 386"
 sudo dpkg --add-architecture i386
+echo ">> Update"
 apt-get update -qq
 #sudo apt-get install -qq libpam-ldap:i386
+echo ">> stdc"
 sudo apt-get install libstdc++6-4.4-pic -y
+echo ">> stdc-32"
 sudo apt-get install lib32stdc++6 -y
+echo ">> ldap"
 sudo apt-get install libpam-ldap:i386 -y -qq
+echo ">> libaio1"
 sudo apt-get install libaio1
+echo ">> rpm"
 sudo apt-get install rpm
+echo "Finished"
